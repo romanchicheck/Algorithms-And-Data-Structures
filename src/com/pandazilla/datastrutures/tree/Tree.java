6d5d8fb729733431aa26f4adc5d1903d17b149f5
rnovikov <v-novir-gri@ae.com>
2017-11-06 23:04:10 +0200

    removing node from tree

--- src/com/pandazilla/datastrutures/tree/Tree.java
+++ src/com/pandazilla/datastrutures/tree/Tree.java
@@ -50,8 +50,81 @@ public class Tree {
         return current;
     }
 
-    public void delete(int id) {
+    public boolean delete(int key) {
+        Node current = root;
+        Node parent = root;
+        boolean isLeftChild = true;
+        while (current.getKey() != key) {
+            parent = current;
+            if (key < current.getKey()) {
+                isLeftChild = true;
+                current = current.getLeftChild();
+            } else {
+                isLeftChild = false;
+                current = current.getRightChild();
+            }
+            if (current == null) {
+                return false;
+            }
+        }
 
+        if (current.getLeftChild() == null && current.getRightChild() == null) {
+            deleteNodeWithoutChildren(current, parent, isLeftChild);
+        } else if (current.getRightChild() == null) {
+            deleteNodeWithOneChild(current, current.getLeftChild(), parent, isLeftChild);
+        } else if (current.getLeftChild() == null) {
+            deleteNodeWithOneChild(current, current.getRightChild(), parent, isLeftChild);
+        } else {
+            deleteNodeWithBothChildren(current, parent, isLeftChild);
+        }
+        return true;
+    }
+
+    private void deleteNodeWithoutChildren(Node current, Node parent, boolean isLeftChild) {
+        if (current == root) {
+            root = null;
+        } else if (isLeftChild) {
+            parent.setLeftChild(null);
+        } else {
+            parent.setRightChild(null);
+        }
+    }
+
+    private void deleteNodeWithOneChild(Node current, Node child, Node parent, boolean isLeftChild) {
+        if (current == root) {
+            root = child;
+        } else if (isLeftChild) {
+            parent.setLeftChild(child);
+        } else {
+            parent.setRightChild(child);
+        }
+    }
+
+    private void deleteNodeWithBothChildren(Node current, Node parent, boolean isLeftChild) {
+        Node successor = getSuccessor(current);
+        if (current == root) {
+            root = successor;
+        } else if (isLeftChild) {
+            parent.setLeftChild(successor);
+        } else {
+            parent.setRightChild(successor);
+        }
+    }
+
+    private Node getSuccessor(Node delNode) {
+        Node successorParent = delNode;
+        Node successor = delNode;
+        Node current = delNode.getRightChild();
+        while (current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.getLeftChild();
+        }
+        if (successor != delNode.getRightChild()) {
+            successorParent.setLeftChild(successor.getRightChild());
+            successor.setRightChild(delNode.getRightChild());
+        }
+        return successor;
     }
 
     public Node maximum() {
@@ -83,6 +156,7 @@ public class Tree {
      * Preorder traversal is also used to get prefix expression on of an expression tree.
      * Please see http://en.wikipedia.org/wiki/Polish_notation to know why prefix expressions are useful.
      * Example: Preorder traversal for the above given figure is 1 2 4 5 3.
+     *
      * @param localRoot
      */
     private void preOrder(Node localRoot) {
@@ -105,6 +179,7 @@ public class Tree {
      * a variation of Inorder traversal
      * where Inorder itraversal s reversed, can be used.
      * Example: Inorder traversal for the above given figure is 4 2 5 1 3.
+     *
      * @param localRoot
      */
     private void inOrder(Node localRoot) {
@@ -125,6 +200,7 @@ public class Tree {
      * Please see the question for deletion of tree for details.
      * Postorder traversal is also useful to get the postfix expression of an expression tree.
      * Please see http://en.wikipedia.org/wiki/Reverse_Polish_notation to for the usage of postfix expression.
+     *
      * @param localRoot
      */
     private void postOrder(Node localRoot) {
