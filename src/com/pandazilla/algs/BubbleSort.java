b441e712162d6a58a70caeec509714f0691b5c73
rnovikov <v-novir-gri@ae.com>
2017-11-10 11:48:52 +0200

    bubble sort

--- src/com/pandazilla/algs/BubbleSort.java
+++ src/com/pandazilla/algs/BubbleSort.java
@@ -4,22 +4,15 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] arr = {4, 4, 9, 2, 3};
-        for(int i = 0;i<arr.length;i++) {
-            int min = arr[i];
-            int min_i = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < min) {
-                    min = arr[j];
-                    min_i = j;
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
                 }
             }
-            if (i != min_i) {
-                int tmp = arr[i];
-                arr[i] = arr[min_i];
-                arr[min_i] = tmp;
-            }
         }
-
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
--- src/com/pandazilla/datastrutures/heap/Heap.java
+++ src/com/pandazilla/datastrutures/heap/Heap.java
@@ -174,5 +174,25 @@ public class Heap {
         }
     }
 
-
+    public static void main(String args[]) {
+        Heap newHeap = new Heap(7);
+        newHeap.generateFilledArray(90);
+        // Print out the array before it is sorted
+        System.out.println("Original Array");
+        System.out.println(Arrays.toString(newHeap.heapArray));
+        System.out.println();
+        newHeap.printTree(4);
+        System.out.println();
+        for (int j = newHeap.maxSize / 2 - 1; j >= 0; j--) {
+            newHeap.heapTheArray(j);
+        }
+        System.out.println("Heaped Array");
+        System.out.println(Arrays.toString(newHeap.heapArray) + "\n");
+        newHeap.printTree(4);
+        System.out.println("HEAPED SORTED");
+        newHeap.heapSort();
+        // Print the sorted array
+        System.out.println("\nSorted Array");
+        System.out.println(Arrays.toString(newHeap.heapArray));
+    }
 }
